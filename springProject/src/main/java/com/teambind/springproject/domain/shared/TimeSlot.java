bd3973cf0332039a3daae455d85081a2b9d7de2e
ddingjoo <ddingsha9@teambind.co.kr>
2025-11-08 22:22:37 +0900

    FEAT TimeSlot Enum 구현 및 테스트
    - 시간 단위를 표현하는 Enum
    - HOUR: 60분, HALFHOUR: 30분
    - 룸 정보 서비스의 TimeSlot과 동일한 구조
    - getMinutes(), getHours(), isHour(), isHalfHour() 메서드 제공
    - 8개 단위 테스트 작성
    
    Related to #6

---
+++ springProject/src/main/java/com/teambind/springproject/domain/shared/TimeSlot.java
@@ -0,0 +1,32 @@
+package com.teambind.springproject.domain.shared;
+
+/**
+ * 시간 단위를 표현하는 Enum.
+ * 룸 서비스의 TimeSlot과 동일한 값을 사용합니다.
+ */
+public enum TimeSlot {
+  HOUR(60),
+  HALFHOUR(30);
+
+  private final int minutes;
+
+  TimeSlot(final int minutes) {
+    this.minutes = minutes;
+  }
+
+  public int getMinutes() {
+    return minutes;
+  }
+
+  public int getHours() {
+    return minutes / 60;
+  }
+
+  public boolean isHour() {
+    return this == HOUR;
+  }
+
+  public boolean isHalfHour() {
+    return this == HALFHOUR;
+  }
+}
